0f8fd6bb8d14552a933f40b36099e141429e1960
Patrick Morton <patrickmorton@Patricks-iMac.local>
2015-09-14 18:37:34 -0400

    added fiels for super cool

---
+++ Exercise 2.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var one = 15
+var two = 5 * 1
+var three = one + two
+var four = 9 * 5
+var five = one - two
+var six = one / two
+var patty = "patty'" , alyssa = "alyssa", chandra = "chandra"
+let garmin = "garmin", mouse = 6, tea = "tea"
+
+var sexy:Double
+
+var poopy:Int = 423
+
+
+var string = "hello"
+var string2 = "World"
+
+var world = string + string2
+var world2 = "\(string) \(string2)"
+
+
+
+
+
---
+++ variables-types.playground/Contents.swift
@@ -0,0 +1,51 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var helloworld = "Hello, playground" // String
+//helloworld += "I'm so cool"
+helloworld = helloworld + "I'm so cool!"
+
+
+var numA = 1.5//Double
+var numB: Float = 1.5 //Float
+var numC = 3 //Int
+
+var sum = 5 + 6
+var product = 5*5
+var divisor = 20/5
+var remainder = 22 % 5
+var sub = 44 - 22
+
+var crazyRsult = sum * product + (divisor * remainder - sub)
+
+var newStr = "Hey " + "how are you?"
+var firstName = "Jon"
+var lastName = "Smith"
+
+var fullname = "\(firstName) \(lastName)"
+
+var price = "Price: $\(numA)"
+
+price = "banana"
+price = "1.0"
+
+let total = 5.1 * 6.8
+
+var jack = 0, jill = 1, bob = 2
+
+jill = 6
+
+let beaver = "beaver", stever = "stever", cleaver = "cleaver"
+
+var totalPrice: Double
+
+totalPrice = 5.5
+
+var sumStr: String = "This is a String"
+
+
+
+
+
+
